7d4a6a3a1d520946700a6971b8206a7d8fea6eb6
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-04 02:29:42 +0000

    feat: parse files formkazani feed

--- transkript-kazani/src/services/transcription/transcription.service.ts
+++ transkript-kazani/src/services/transcription/transcription.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { Config } from "src/config";
+import { OpenaiService } from "../openai/openai.service";
 import { RssParserService } from "../rss-parser/rss-parser.service";
 
 @Injectable()
@@ -7,9 +8,21 @@ export class TranscriptionService {
 	constructor(
 		private readonly rssParser: RssParserService,
 		private readonly config: Config,
+		private openai: OpenaiService,
 	) {}
 
-	getFilesForTranscription() {
-		const feed = this.rssParser.parseFeed();
+	async transcribe() {
+		const files = await this.getFilesForTranscription();
+		console.log(files);
+		for (const file of files) {
+			// const transcription = await this.openai.transcribe(file);
+			// console.log(transcription);
+		}
+	}
+
+	private async getFilesForTranscription() {
+		const feed = await this.rssParser.parseFeed(this.config.kazani.feedUrl);
+
+		return feed.items.slice(1, 1).map((item) => item.enclosure.url);
 	}
 }
